5678805caf83ab7875f49fa422350b8eba0e86a7
Logan-kennard <logan.kennard@bsu.edu>
2026-03-24 17:28:29 +0000

    Hex conversion done

--- Test.java
+++ Test.java
@@ -1,13 +1,34 @@
 import java.util.Scanner;
 public class Test {
+static final String[] hex= {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
 public static void main(String[] args) {
 Scanner input= new  Scanner(System.in);
 System.out.println("Enter a number:");
 int num1=input.nextInt();
 int num2=input.nextInt();
-System.out.println("Your numbers are");
-System.out.println(num1+num2);
+System.out.println("Your numbers are " + num1 + " and " + num2);
+int sum=num1+num2;
+System.out.println("The sum is " + sum);
 input.close();
-int sum= num1 + num2;
-System.out.println(sum);}
+String numSting1=Integer.toString(num1);
+String numSting2=Integer.toString(num2);
+String sumString=Integer.toString(sum);
+String hex1=toHex(num1);
+String hex2=toHex(num2);
+String hexSum=toHex(sum);
+System.out.println("Hexadecimal values:");
+System.out.println("Number 1: 0x" + hex1);
+System.out.println("Number 2: 0x" + hex2);
+System.out.println("Sum: 0x" + hexSum);
+}
+private static String toHex(int num) {
+String hexString="";
+while(num>0) {
+int rem=num%16;
+hexString=hex[rem]+hexString;
+num=num/16;
+}
+
+return hexString;
+}
 }
