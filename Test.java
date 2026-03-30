e418e5a42dfe65c7a438f76dfec2999aa8e4e065
Logan-kennard <logan.kennard@bsu.edu>
2026-03-30 17:13:00 +0000

    Part 2 Done

--- Test.java
+++ Test.java
@@ -10,16 +10,23 @@ System.out.println("Your numbers are " + num1 + " and " + num2);
 int sum=num1+num2;
 System.out.println("The sum is " + sum);
 input.close();
-String numSting1=Integer.toString(num1);
-String numSting2=Integer.toString(num2);
-String sumString=Integer.toString(sum);
+// Convert to hexadecimal
 String hex1=toHex(num1);
 String hex2=toHex(num2);
 String hexSum=toHex(sum);
+// Object code 
+System.out.println("Object code for:");
+System.out.println("C000"+ hex1);
+System.out.println("6000"+ hex2);
+System.out.println("E1000A");
+System.out.println("00");
+System.out.println("0000");
+// Display hexadecimal values
 System.out.println("Hexadecimal values:");
 System.out.println("Number 1: 0x" + hex1);
 System.out.println("Number 2: 0x" + hex2);
 System.out.println("Sum: 0x" + hexSum);
+
 }
 private static String toHex(int num) {
 String hexString="";
@@ -28,7 +35,6 @@ int rem=num%16;
 hexString=hex[rem]+hexString;
 num=num/16;
 }
-
-return hexString;
+ return hexString;
 }
 }
